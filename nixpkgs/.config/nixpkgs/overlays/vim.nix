a4fca7637143ddaec4fd4941af3aa6e51c54f198
Anton Schirg <anton.schirg@posteo.de>
2018-02-17 20:55:39 +0100

    vim: header switch and .editorconfig

--- nixpkgs/.config/nixpkgs/overlays/vim.nix
+++ nixpkgs/.config/nixpkgs/overlays/vim.nix
@@ -72,7 +72,33 @@ in
       " Buffer switching
       nnoremap <C-l> :bnext<CR>
       nnoremap <C-h> :bprev<CR>
-      nnoremap <C-c> :bn<bar>bd#<CR> " switch to next and close last buffer
+      nnoremap <C-c> :bp<bar>bd#<CR> " switch to previous and close last buffer (closing current closes window)
+
+      " Switch between header and implementation
+      "
+      command! CtrlPSameName call feedkeys(":CtrlP\<cr>".expand('%:t:r'), "t")
+      " Source: http://vim.wikia.com/wiki/Easily_switch_between_source_and_header_file#By_modifying_ftplugins
+      function <SID>SwitchSourceHeader()
+          try
+              if (expand("%:t") == expand("%:t:r") . ".h")
+                  try
+                      find %:t:r.cpp
+                  catch /^Vim\%((\a\+)\)\=:E345/
+                      " Error: Can't find file. Try inline instead.
+                      find %:t:r.inl
+                  endtry
+              else
+                  find %:t:r.h
+              endif
+          catch /^Vim\%((\a\+)\)\=:E345/
+              " If we can't find it in the path, see if it's in ctrlp.
+              if exists(":CtrlPSameName") == 2
+                  CtrlPSameName
+              endif
+          endtry
+      endfunction
+
+      nnoremap <leader>h :call <SID>SwitchSourceHeader()<CR>
     '';
 
     vimrcConfig.vam.knownPlugins = super.vimPlugins // customPlugins;
@@ -84,6 +110,7 @@ in
             "The_NERD_tree"
             "ack-vim"
             "vim-buftabline"
+            "editorconfig-vim"
         ]; }
     ];
   };
